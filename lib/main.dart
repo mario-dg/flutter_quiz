28658a5d00154df284af673f91ab4f99acb5c7ef
mario-dg <mario.dagraca@gmx.com>
2023-09-11 19:03:24 +0200

    Added new Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Quiz());
+}
+
+class Quiz extends StatelessWidget {
+  const Quiz({super.key});
+
+  @override
+  Widget build(context) {
+    return const Scaffold(body: Text("Hello World"));
+  }
+}
